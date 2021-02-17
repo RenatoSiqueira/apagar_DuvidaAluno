8a34c1a7311062bf1d0a900e3699668b93b2e7cf
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-02-17 09:24:20 -0300

    teste

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,21 @@
-
-import React from 'react'
-import App from 'next/app'
-import '../css/tailwindcss.css'
-
+import React from "react";
+import App from "next/app";
+// import '../css/tailwindcss.css'
+import "tailwindcss/tailwind.css";
 
 class MyApp extends App {
-    render() {
-        const { Component, pageProps } = this.props
-        return ( 
-          <React.Fragment>
-                <style jsx global>
-                {`
-                    @import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;600;700&display=swap');
-                `}
-                </style>
-            <Component {...pageProps} />
-          </React.Fragment>
-        )
-    }
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <React.Fragment>
+        <style jsx global>
+          {`
+            @import url("https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;600;700&display=swap");
+          `}
+        </style>
+        <Component {...pageProps} />
+      </React.Fragment>
+    );
+  }
 }
-export default MyApp
+export default MyApp;
