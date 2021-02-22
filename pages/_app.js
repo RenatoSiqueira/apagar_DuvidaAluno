6544b98964b6b4e145e0023310433e61058c301d
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-02-22 11:58:00 -0300

    teste 2

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,16 @@ class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <React.Fragment>
+        <style jsx global>
+          {`
+            @font-face {
+              src: url("https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap");
+            }
+            * {
+              font-family: "Inter", sans-serif;
+            }
+          `}
+        </style>
         <Component {...pageProps} />
       </React.Fragment>
     );
